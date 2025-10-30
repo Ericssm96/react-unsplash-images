567c1f6a29bedaf9d07a3bcd9d956433b55f1069
Eric Stéfano <ericssm96@gmail.com>
2025-10-29 22:40:16 -0300

    app context created

--- src/context.jsx
+++ src/context.jsx
@@ -1,1 +1,17 @@
 import { useState, useEffect, createContext, useContext } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({children}) => {
+  const greeting = "hello world";
+
+  return (
+    <AppContext value={{greeting}}>
+      {children}
+    </AppContext>
+  )
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+}
