5a9496f1835f7cd8c5890c9b540073677ca6add3
Eric Stéfano <ericssm96@gmail.com>
2025-11-03 18:58:32 -0300

    finished setting up react query and rendering of images fetched in the browser

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,18 +3,46 @@ import { useQuery } from "@tanstack/react-query";
 
 export const Gallery = () => {
 
-  const {isError, isLoading, data} = useQuery({
+  const {isError, isLoading, data: responseData} = useQuery({
     queryKey: ["search-photos"],
     queryFn: () => {
       return customFetch("/search/photos?page=1&query=cat");
     }
-  })
+  });
 
-  if(!isLoading) {
-    console.log(data.data.results);
+  if(isLoading) return (
+    <section className="image-container">
+      <h4>Loading...</h4>
+    </section>
+  );
+
+  if(isError) return (
+    <section className="image-container">
+      <h4>There was an error...</h4>
+    </section>
+  )
+
+  const data = responseData.data;
+  const imgArr = data.results;
+
+  if(data.total === 0) {
+    return (
+      <section className="image-container">
+        <h4>Nothing was found</h4>
+      </section>
+    )
   }
-
+  
   return (
-    <div>Gallery</div>
+    <section className="image-container">
+      {imgArr.map((imgData)=>{
+        const {id, alt_description: altDescription, urls} = imgData;
+        const regularSizeUrl = urls.regular;
+
+        return (
+          <img className="img" src={regularSizeUrl} alt={altDescription} key={id} />
+        )
+      })}
+    </section>
   )
 }
