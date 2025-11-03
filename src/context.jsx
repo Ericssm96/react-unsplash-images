f9a5ad14e43d30c246832a9885348817ae99cbc2
Eric Stéfano <ericssm96@gmail.com>
2025-11-03 20:35:30 -0300

    added check for user theme preference

--- src/context.jsx
+++ src/context.jsx
@@ -2,16 +2,26 @@ import { useState, useEffect, createContext, useContext } from "react";
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia("(prefers-color-scheme:dark)").matches;
+
+  console.log(prefersDarkMode)
+
+  return prefersDarkMode;
+}
+
 export const AppProvider = ({children}) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState("space");
 
   const toggleDarkTheme = () => {
     const scopedIsDarkTheme = !isDarkTheme;
     setIsDarkTheme(scopedIsDarkTheme);
-    const body = document.querySelector("body");
-    body.classList.toggle("dark-theme", scopedIsDarkTheme);
   }
+
+  useEffect(()=>{
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
 
   return (
     <AppContext.Provider value={{isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm}}>
