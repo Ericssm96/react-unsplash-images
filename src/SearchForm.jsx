787bb97e2de5756a0064912dc25846eac1d93cbd
Eric Stéfano <ericssm96@gmail.com>
2025-11-03 11:34:54 -0300

    started setting up search logic

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,5 +1,18 @@
 export const SearchForm = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if(!searchValue) return;
+  }
+
   return (
-    <div>SearchForm</div>
+    <section>
+      <h1 className="title">Unsplash Images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input type="text" className="form-input search-input" placeholder="Cat" name="search" />
+        <button type="submit" className="btn">search</button>
+      </form>
+    </section>
   )
 }
