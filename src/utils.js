8893364b2e2bc67b751360fa3539267cf1922d05
Eric Stéfano <ericssm96@gmail.com>
2025-11-03 21:18:20 -0300

    added dotenv variable to the api key

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const customFetch = axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID i2QzMU632_zktCP4qk4JHl2GxRvhXz5rRhZ1wiBDnhM",
+    Authorization: `Client-ID ${import.meta.env.VITE_API_KEY}`,
     "Accept-Version": "v1"
   }
 });
